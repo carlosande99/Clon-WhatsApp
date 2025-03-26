231d2c1debe8dc6f83009b711e4c0611925fe4ec
carlosande99 <carlosandemetrioguerrero@gmail.com>
2025-03-26 23:41:42 +0100

    creado primeras paginas

---
+++ client/js/registro.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const data = {
+        nombre: formData.get('nombre'),
+        email: formData.get('email'),
+        password: formData.get('password'),
+    };
+    
+    fetch('/registro', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res => {
+        if(res.ok) {
+            alert('usuario creado');
+            // window.location.href = '/';
+        } else {
+            alert(json.error);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Error al procesar la solicitud');
+    });
+});
